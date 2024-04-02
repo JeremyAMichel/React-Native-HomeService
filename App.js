b9def071a58e5351ffdfd966370b8bf17ab96915
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-04-02 16:19:52 +0200

    fix issue with OAuth

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     >
       <View style={styles.container}>
         <SignedIn>
-          you are sign in
+          <Text>you are sign in</Text>
         </SignedIn>
         <SignedOut>
           <Login />
