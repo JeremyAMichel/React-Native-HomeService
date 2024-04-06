28f4a63cebaa2961e0742a10245223e680736d7a
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-04-06 19:29:12 +0200

    add slider to front

--- App/Screens/HomeScreen/Home.jsx
+++ App/Screens/HomeScreen/Home.jsx
@@ -1,14 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Header from './Header'
-import Slider from './Slider'
+import { View, Text } from "react-native";
+import React from "react";
+import Header from "./Header";
+import Slider from "./Slider";
 
 export default function Home() {
-  
   return (
     <View>
       <Header />
-      <Slider />
+      <View style={{padding: 20}}>
+        <Slider />
+      </View>
     </View>
-  )
+  );
 }
